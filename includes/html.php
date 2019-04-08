9fc2bbeb44a36e267771fa0c4fa409481f5b1934
Salomé Leroux <leroux.salome@orange.fr>
2019-04-08 15:38:06 +0200

    Création htlm.php

---
+++ includes/html.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./assets/css/bootstrap.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+        <title>Portfolio - Salomé Leroux </title>
+  </head>
+  
