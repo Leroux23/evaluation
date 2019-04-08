d8a5417640ea94fceea1d7baec08434d3fc39454
Salomé Leroux <leroux.salome@orange.fr>
2019-04-08 15:38:18 +0200

    Création header.php

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #b38184">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php?page=accueil">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?page=experiences">Expériences</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="index.php?page=diplomes" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Diplômes
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="index.php?page=certification">Certification Référent Digital</a>
+              <a class="dropdown-item" href="index.php?page=licencepro">Licence professionnelle</a>
+              <a class="dropdown-item" href="index.php?page=licenceaes">Licence AES</a>
+              <div class="dropdown-divider"></div>
+            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?page=mecontacter">Me contacter</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Rechercher">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+        </form>
+      </div>
+    </nav>
+  </header>
+  <script type="text/javascript" src="./assets/js/jquery.js"></script>
+  <script type="text/javascript" src="./assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
