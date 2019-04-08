10bc8014f36f8c5d9690366ce0511bb72b5062de
Salomé Leroux <leroux.salome@orange.fr>
2019-04-08 15:36:56 +0200

    Création index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<?php
+date_default_timezone_set('Europe/Paris');
+setlocale(LC_CTYPE, 'fr_FR.UTF-8');
+include_once "./includes/html.php";
+include_once "./includes/header.php";
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+else {
+    $page = "accueil";
+}
+$path = "./includes/";
+$contenu = glob($path . "*.inc.php");
+$page = $path . $page . ".inc.php";
+if (in_array($page, $contenu)) {
+    include_once $page;
+}
+else {
+    include_once "./includes/accueil.inc.php";
+}
+include_once "./includes/footer.php";
+
+
+
+
+
+
+
+ ?>
