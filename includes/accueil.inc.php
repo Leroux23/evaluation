0b9b226cb139306705f6cd9d6e11c2a99489fb2b
Salomé Leroux <leroux.salome@orange.fr>
2019-04-08 15:40:38 +0200

    Création accueil.inc.php

---
+++ includes/accueil.inc.php
@@ -0,0 +1,16 @@
+<h1>Accueil</h1>
+    <section class="page-section clearfix">
+    <div class="container">
+      <div class="intro">
+        <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="./assets/pictures/IMG-9607.jpg" alt="">
+        <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+          <h2 class="section-heading mb-4">
+            <span class="section-heading-lower">Bienvenue</span>
+          </h2>
+          <p class="mb-3">Récemment diplômée d'une certification de référent digital, je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles.
+Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme, la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
