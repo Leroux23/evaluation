88704f04df1c573e07fe511c16c390c18a520bd5
Salomé Leroux <leroux.salome@orange.fr>
2019-04-11 19:56:31 +0200

    Intégration d'articles.

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,14 +1,30 @@
   <title> Salomé Leroux </title>
     <body>
       <main>
-        <p>Récemment diplômée d'une certification de référent digital, je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles. Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme, la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux.
-        </p>
-        <p>Récemment diplômée d'une certification de référent digital, je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles. Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme, la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux.
-        </p>
-        <p>Récemment diplômée d'une certification de référent digital, je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles. Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme, la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux.
-        </p>
-        <p>Récemment diplômée d'une certification de référent digital, je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles. Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme, la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux.
-        </p>
-        <p>Récemment diplômée d'une certification de référent digital, je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles. Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme, la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux.
-        </p>
+        <div class="articles">
+        <div class="col-lg-4">
+          <img class="rounded" src="./assets/pictures/IMG_1961.jpg" alt="generic placeholder image" width="140" height="93">
+          <h2>Edito</h2>
+          <p>Récemment diplômée d'une certification de référent digital,
+            je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles.
+            Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme,
+            la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux. </p>
+          </div>
+            <div class="col-lg-4">
+              <img class="rounded" src="./assets/pictures/IMG_1961.jpg" alt="generic placeholder image" width="140" height="93">
+              <h2>Edito</h2>
+              <p>Récemment diplômée d'une certification de référent digital,
+                je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles.
+                Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme,
+                la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux. </p>
+              </div>
+              <div class="col-lg-4">
+                <img class="rounded" src="./assets/pictures/IMG_1961.jpg" alt="generic placeholder image" width="140" height="93">
+                <h2>Edito</h2>
+                <p>Récemment diplômée d'une certification de référent digital,
+                  je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles.
+                  Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme,
+                  la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux. </p>
+                </div>
+              </div>
       </main>
