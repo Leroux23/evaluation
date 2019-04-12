baee85dcef20559ca1cfdcac22de1a2a0ff662fa
Salomé Leroux <leroux.salome@orange.fr>
2019-04-12 14:23:55 +0200

    Rédaction contenus.

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -11,20 +11,19 @@
             la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux. </p>
           </div>
             <div class="col-lg-4">
-              <img class="rounded" src="./assets/pictures/IMG_1961.jpg" alt="generic placeholder image" width="140" height="93">
-              <h2>Edito</h2>
-              <p>Récemment diplômée d'une certification de référent digital,
-                je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles.
-                Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme,
-                la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux. </p>
+              <img class="rounded" src="./assets/pictures/fashion.jpg" alt="generic placeholder image" width="140" height="93">
+              <h2>Mode</h2>
+              <p>Depuis de nombreuses annnées, je m'intéresse à la mode. En effet, les magazines de mode sont intéressants à analyser car nous pouvons nous rendre des différences culturelles du monde.
+                D'un pays à un autre, les tendances vestimanaires divergent. La religion, la politique, la géographie et l'actualité font partis des critères qui entrent en jeu pour les stylistes.
+                L'intérêt porté sur la mode m'a aidé à développer ma créativité du point de vue rédactionnel et visuel.
+          </p>
               </div>
               <div class="col-lg-4">
-                <img class="rounded" src="./assets/pictures/IMG_1961.jpg" alt="generic placeholder image" width="140" height="93">
-                <h2>Edito</h2>
-                <p>Récemment diplômée d'une certification de référent digital,
-                  je vous présente l'ensemble de mes compétences que j'ai pu acquérir durant mes études et mes expériences professionnelles.
-                  Désormais, j'aspire à me spécialiser dans le community management qui intègre une grande partie des compétences acquises, comme,
-                  la rédaction web, la création de visuel, le développement de l'image de marque d'une entreprise sur les réseaux sociaux. </p>
+                <img class="rounded" src="./assets/pictures/news.jpg" alt="generic placeholder image" width="140" height="93">
+                <h2>Lire</h2>
+                <p>Dès mon plus jeune âge, mes parents m'ont appris à m'instruire. Cette éducation m'a aidé à étendre ma curiosité sur une diversité de sujets, tels que&nbsp;:&nbsp;
+                les faits historiques,&nbsp;les mouvements économiques,&nbsp;les nouvelles technologies,&nbsp;l'actualité française et du monde.
+                Avec le recul, la lecture m'a permis d'éveiller ma culture générale ainsi de forger mon propre opinion du monde qui nous entoure.</p>
                 </div>
               </div>
       </main>
