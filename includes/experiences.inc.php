5cbd1b6febb3be555d8f2de6d40f1acff976a6de
Salomé Leroux <leroux.salome@orange.fr>
2019-04-12 11:59:06 +0200

    Intégration du contenu.

--- includes/experiences.inc.php
+++ includes/experiences.inc.php
@@ -1,1 +1,107 @@
-<h1>Expériences</h1>
+<section class="page-section about-heading">
+      <div class="container">
+        <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="./assets/pictures/cvs.jpg" alt="">
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <span class="section-heading-upper">Caux Seine Agglo</span>
+                </h2>
+                <h3>
+                  <span class="section-heading-lower">Assistante documentaliste - Octobre 2017 | Janvier 2019</span>
+                </h3>
+                <ul>
+                  <li>Développement d'un portail documentaire à partir du logiciel PMB |<a href="https://pmb.cauxseine.fr/">&nbsp;Cliquez ici</a></li>
+                  <li>Alimentation de la base de données</li>
+                  <li>Réalisation de produits documentaires (panorama de presse, revue de presse...)</li>
+                  <li>Veille documentaire</li>
+                  <li>Gestion des recherches documentaires des collaborateurs</li>
+                  <li>Déclarations et diffusions des demandes de copies</li>
+                  <li>Conduite des abonnements</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="page-section about-heading">
+          <div class="container">
+            <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="./assets/pictures/logo-rouen-expo-evenements.jpg" alt="">
+            <div class="about-heading-content">
+              <div class="row">
+                <div class="col-xl-9 col-lg-10 mx-auto">
+                  <div class="bg-faded rounded p-5">
+                    <h2 class="section-heading mb-4">
+                      <span class="section-heading-upper">Rouen Expo Evénements</span>
+                    </h2>
+                    <h3>
+                      <span class="section-heading-lower">Assistante de direction - Janvier 2017 | Février 2017</span>
+                    </h3>
+                    <ul>
+                      <li>Gestion du plan de formations</li>
+                      <li>Organisation des formations</li>
+                      <li>Gestion du budget disponible</li>
+                      <li>Veille documentaire</li>
+                      <li>Mis à jour du rapport d'activité 2016</li>
+                      <li>Déclarations des manifestations</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="page-section about-heading">
+              <div class="container">
+                <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="./assets/pictures/desjardins.png" alt="">
+                <div class="about-heading-content">
+                  <div class="row">
+                    <div class="col-xl-9 col-lg-10 mx-auto">
+                      <div class="bg-faded rounded p-5">
+                        <h2 class="section-heading mb-4">
+                          <span class="section-heading-upper">Desjardins</span>
+                        </h2>
+                        <h3>
+                          <span class="section-heading-lower">Hôtesse de caisse (emploi étudiant) - Novembre 2016 | Février 2017</span>
+                        </h3>
+                        <ul>
+                          <li>Gestion de la caisse entrée/sortie</li>
+                          <li>Accueil des clients</li>
+                          <li>Mise en rayon</li>
+                          <li>Nettoyage des caisses</li>
+                          <li>Emballage des achats des clients</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="page-section about-heading">
+                  <div class="container">
+                    <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="./assets/pictures/wings-for-life.png" alt="">
+                    <div class="about-heading-content">
+                      <div class="row">
+                        <div class="col-xl-9 col-lg-10 mx-auto">
+                          <div class="bg-faded rounded p-5">
+                            <h2 class="section-heading mb-4">
+                              <span class="section-heading-upper">Wings for life world run</span>
+                            </h2>
+                            <h3>
+                              <span class="section-heading-lower">Bénévole | Mai 2016</span>
+                            </h3>
+                            <p>Wings for Life est une fondation à but non lucratif ayant pour but de soigner les lésions de la moelle épinière.
+                              Le but de cette course est de récolter des fonds afin de faire évoluer la recherche médicale pour les lésions de la moelle épinière.
+                              Par ailleurs, cet événement a lieu tous les ans, partout dans le monde et le départ est à la même heure malgré les décalages horaires.
+                              <ul>
+                                <li>Spécialité de cette course&nbsp;: c'est la ligne d'arrivée qui vient à vous.</li>
+                              </ul>
+                             </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
