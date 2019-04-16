c63bd3f387c4532597cdd43be25ccebdc97ad025
Salomé Leroux <leroux.salome@orange.fr>
2019-04-16 10:07:25 +0200

    Intégration du contenu.

--- includes/licenceaes.inc.php
+++ includes/licenceaes.inc.php
@@ -1,1 +1,57 @@
-<h1>Licence Administration Economique et Sociale</h1>
+<h2>Licence Administration Economique et Sociale</h2>
+<h3>Septembre 2014 | Juillet 2017</h3>
+<img src="./assets/pictures/univ.jpg" width="200" height="72">
+<main>
+  <div class="articles">
+  <div class="col-lg-4">
+    <img class="rounded" src="./assets/pictures/fluctuation.png" alt="generic placeholder image" width="100" height="100">
+    <h2>Economie</h2>
+    <p class="certification">
+    <ul>
+      <li>Economie d'entreprise</li>
+      <li>Analyse économique</li>
+      <li>Gestion de l'entreprise</li>
+      <li>Problèmes économiques contemporains</li>
+      <li>Théories du commerce international</li>
+      <li>Relations économiques internationales</li>
+      <li>Economie du travail et des ressources humaines</li>
+      <li>Economie de l'Union Européenne</li>
+      <li>Economie du secteur non marchand</li>
+    </ul>
+  </p>
+    </div>
+      <div class="col-lg-4">
+        <img class="rounded" src="./assets/pictures/balance.png" alt="generic placeholder image" width="100" height="100">
+        <h2>Droit</h2>
+        <p class="certification">
+          <ul>
+              <li>Institutions publiques</li>
+            <li>Droit privé</li>
+            <li>Droit des obligations</li>
+            <li>Droit administratif</li>
+            <li>Droit de la responsabilité</li>
+            <li>Droit fiscal</li>
+            <li>Droit de l'Union Européenne</li>
+            <li>Droit des sociétés</li>
+            <li>Droit social</li>
+            <li>Droit du travail</li>
+          </ul>
+        </p>
+        </div>
+        <div class="col-lg-4">
+          <img class="rounded" src="./assets/pictures/debt.png" alt="generic placeholder image" width="100" height="100">
+          <h2>Gestion</h2>
+          <p class="certification">
+            <ul>
+              <li>Statistiques probabilistes</li>
+              <li>Comptabilité</li>
+              <li>Analyse financière</li>
+              <li>Contrôle de gestion</li>
+              <li>Etude des entreprises</li>
+              <li>Problèmes managériaux contemporains</li>
+              <li>Gestion informatisée des entreprises | Pack office</li>
+            </ul>
+          </p>
+          </div>
+        </div>
+</main>
--- includes/licencepro.inc.php
+++ includes/licencepro.inc.php
@@ -4,7 +4,6 @@ parcours Documentaliste d’entreprise et métiers de l’Infodoc</h2>
 <img src="./assets/pictures/ifa.png" width="140" height="144">
 <section class="page-section about-heading">
       <div class="container">
-        <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="./assets/pictures/organization.png" alt="">
         <div class="about-heading-content">
           <div class="row">
             <div class="col-xl-9 col-lg-10 mx-auto">
@@ -13,9 +12,11 @@ parcours Documentaliste d’entreprise et métiers de l’Infodoc</h2>
                   <span class="section-heading-upper">Management</span>
                 </h2>
                 <ul>
-                  <li></li>
+                  <li>Organisation d'entreprise et besoins d'information</li>
                   <li>Evolution des métiers de l'Infodoc</li>
                   <li>Gestion de projets</li>
+                  <li>Gestion des connaissances</li>
+                  <li>Droit de l'information</li>
                 </ul>
               </div>
             </div>
@@ -23,3 +24,120 @@ parcours Documentaliste d’entreprise et métiers de l’Infodoc</h2>
         </div>
       </div>
     </section>
+    <div class="container">
+      <div class="about-heading-content">
+        <div class="row">
+          <div class="col-xl-9 col-lg-10 mx-auto">
+            <div class="bg-faded rounded p-5">
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-upper">Structuration de l'information</span>
+              </h2>
+              <ul>
+                <li>Identification des documents</li>
+                <li>Analyse documentaire</li>
+                <li>Langages de description</li>
+                <li>Traitement des documents audivisuels</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="container">
+    <div class="about-heading-content">
+      <div class="row">
+        <div class="col-xl-9 col-lg-10 mx-auto">
+          <div class="bg-faded rounded p-5">
+            <h2 class="section-heading mb-4">
+              <span class="section-heading-upper">Recherche d'information et veille</span>
+            </h2>
+            <ul>
+              <li>Sources d'information</li>
+              <li>Interrogation des serveurs professionnels</li>
+              <li>Outils de veille</li>
+              <li>Pilotage d'un dispositif de veille</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<div class="container">
+  <div class="about-heading-content">
+    <div class="row">
+      <div class="col-xl-9 col-lg-10 mx-auto">
+        <div class="bg-faded rounded p-5">
+          <h2 class="section-heading mb-4">
+            <span class="section-heading-upper">Diffusion et valorisation de l'information</span>
+          </h2>
+          <ul>
+            <li>Produits documentaires</li>
+            <li>Note de synthèse</li>
+            <li>Dossier documentaire électronique</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<div class="container">
+  <div class="about-heading-content">
+    <div class="row">
+      <div class="col-xl-9 col-lg-10 mx-auto">
+        <div class="bg-faded rounded p-5">
+          <h2 class="section-heading mb-4">
+            <span class="section-heading-upper">Système d'information documentaire</span>
+          </h2>
+          <ul>
+            <li>Information structurée</li>
+            <li>Base de données</li>
+            <li>Gestion de contenus</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<div class="container">
+  <div class="about-heading-content">
+    <div class="row">
+      <div class="col-xl-9 col-lg-10 mx-auto">
+        <div class="bg-faded rounded p-5">
+          <h2 class="section-heading mb-4">
+            <span class="section-heading-upper">Gestion de documents d'entreprise</span>
+          </h2>
+          <ul>
+            <li>Archivage</li>
+            <li>Record Management</li>
+            <li>Gestion de projets</li>
+            <li>Gestion électronique des documents (GED)</li>
+            <li>Gestion de la documentation technique</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<div class="container">
+  <div class="about-heading-content">
+    <div class="row">
+      <div class="col-xl-9 col-lg-10 mx-auto">
+        <div class="bg-faded rounded p-5">
+          <h2 class="section-heading mb-4">
+            <span class="section-heading-upper">Web</span>
+          </h2>
+          <ul>
+            <li>Wordpress | Création d'un blog<a href="https://insertionrefugies.wordpress.com/">&nbsp;Cliquez ici</a></li>
+            <li>Joomla! | Création d'un portfolio<a href="http://lerouxninarchi.fr/">&nbsp;Cliquez ici</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
