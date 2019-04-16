14cf48606279e244590a3a790de7e51b156afd64
Salomé Leroux <leroux.salome@orange.fr>
2019-04-16 16:14:51 +0200

    Préparation pour BDD. + Mise en page.

--- includes/frmmecontacter.inc.php
+++ includes/frmmecontacter.inc.php
@@ -1,4 +1,10 @@
 <?php
+$nom = isset($_POST['nom']) ? $_POST['nom'] : "";
+$prenom = isset($_POST['prenom'])? $_POST['prenom'] : "";
+$mail = isset($_POST['mail'])? $_POST['mail'] : "";
+
+
+
 if (!isset($nom)) $nom = "";
 if (!isset($prenom)) $prenom = "";
 if (!isset($mail)) $mail = "";
@@ -7,8 +13,11 @@ if (!isset($mail)) $mail = "";
     $.fancybox.open('<form action="index.php?page=frmmecontacter" method="post">' +
         '<div><label for="nom">Nom&nbsp;: </label><input type="text" id="nom" name="nom" value="<?= $nom ?>" /></div>' +
         '<div><label for="prenom">Prénom&nbsp;: </label><input type="text" id="prenom" name="prenom" value="<?= $prenom ?>" /></div>' +
-        '<div><label for="mail">Adresse email&nbsp;: </label><input type="text" id="mail" name="mail" value="<?=$mail?>" /></div>' +
+        '<div><label for="mail">Adresse mail&nbsp;: </label><input type="text" id="mail" name="mail" value="<?=$mail?>" /></div>' +
         '<div><label for="text">Message&nbsp;: </label><textarea class="form-control" rows="7"></textarea></div>' +
         '<div><input type="reset" value="Effacer" /><input type="submit" value="Envoyer" /></div>' +
         '</form>"');
+        if (window.confirm('Envoyer')) {
+          window.alert('Votre message a bien été envoyé');
+        }
 </script>
