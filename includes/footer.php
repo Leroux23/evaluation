507640b6135d5fe3277ccc8ef770813240ff02d7
Salomé Leroux <leroux.salome@orange.fr>
2019-04-16 14:50:19 +0200

    Création du footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,6 @@
 <script type="text/javascript" src="./assets/js/bootstrap.bundle.js"></script>
+<footer>
+  <p class="m-0 small">Copyright&copy; <?php echo date('Y'); ?> by <img src="./assets/pictures/logosalome.png" width="50" height="50"></p>
+</footer>
 </body>
 </html>
