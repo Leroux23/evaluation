f96283efb15d91509759ae8192e304aaf221da03
Salomé Leroux <leroux.salome@orange.fr>
2019-04-16 14:50:52 +0200

    Modification lien image.

--- includes/coordonnees.inc.php
+++ includes/coordonnees.inc.php
@@ -1,1 +1,20 @@
 <h2>Mes coordonnées</h2>
+<table>
+  <tr>
+    <td><img src="./assets/pictures/pin.png" width="60" height="60"></td>
+    <td>15 impasse du verger<br>
+    76190 Saint-Clair-sur-les-Monts</td>
+  </tr>
+  <tr>
+    <td><img src="./assets/pictures/smartphone.png" width="60" height="60"></td>
+    <td input type="tel">06 21 39 41 73</td>
+  </tr>
+  <tr>
+    <td><img src="./assets/pictures/message.png" width="60" height="60"></td>
+    <td input type="email">leroux.salome@orange.fr</td>
+  </tr>
+  <tr>
+    <td><img src="./assets/pictures/linkedin.png" width="60" height="60"></td>
+    <td><a href="https://www.linkedin.com/in/salom%C3%A9-leroux/" target="_blank">Salomé Leroux</a></td>
+  </tr>
+</table>
