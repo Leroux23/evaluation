1973eb7aa479740cc7f4c607489fb25e8f7e4bf7
Salomé Leroux <leroux.salome@orange.fr>
2019-04-19 16:45:13 +0200

    Responsive + formulaire.

--- includes/frmmecontacter.inc.php
+++ includes/frmmecontacter.inc.php
@@ -47,17 +47,4 @@ $('.fancybox-content').on('click', function() {
       '<div><input type="hidden" name="formulaire" /><input type="reset" value="Effacer" /><input type="submit" value="Envoyer" /></div>' +
       '</form>"');
 });
-
-
-
-    /*$.fancybox.open('<form action="index.php?page=frmmecontacter" method="post">' +
-        '<div><label for="nom">Nom&nbsp;: </label><input type="text" id="nom" name="nom" value="<?= $nom ?>" /></div>' +
-        '<div><label for="prenom">Prénom&nbsp;: </label><input type="text" id="prenom" name="prenom" value="<?= $prenom ?>" /></div>' +
-        '<div><label for="mail">Adresse mail&nbsp;: </label><input type="text" id="mail" name="mail" value="<?=$mail?>" /></div>' +
-        '<div><label for="text">Message&nbsp;: </label><textarea class="form-control" rows="7" value="<?=$message?>"></textarea></div>' +
-        '<div><input type="hidden" name="formulaire" /><input type="reset" value="Effacer" /><input type="submit" value="Envoyer" /></div>' +
-        '</form>"');*/
-        /*if (window.confirm('Envoyer')) {
-          window.alert('Votre message a bien été envoyé');
-        }*/
 </script>
